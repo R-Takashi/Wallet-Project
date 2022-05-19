c0072366438f70077db0b4f134531f5a9cd3e8b1
Rodrigo Takashi Condo <rodrigo0takashi@gmail.com>
2022-05-19 01:20:23 -0300

    Trybe Wallet - Requisito 8

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Form from './components/Form';
 import Header from './components/Header';
 
 class Wallet extends React.Component {
   render() {
+    const { expenses } = this.props;
+
     return (
       <div>
         <Header />
@@ -22,10 +26,39 @@ class Wallet extends React.Component {
               <th>Editar/Excluir</th>
             </tr>
           </thead>
+          <tbody>
+            {expenses.map((expense) => {
+              const currentCurrency = Object.entries(expense.exchangeRates)
+                .find((currency) => currency[0] === expense.currency);
+              const nameCurrency = currentCurrency[1].name.split('/');
+              const currencyValue = currentCurrency[1].ask;
+              const total = expense.value * currencyValue;
+              return (
+                <tr key={ expense.id }>
+                  <td>{expense.description}</td>
+                  <td>{expense.tag}</td>
+                  <td>{expense.method}</td>
+                  <td>{Number(expense.value).toFixed(2)}</td>
+                  <td>{nameCurrency[0]}</td>
+                  <td>{Number(currencyValue).toFixed(2)}</td>
+                  <td>{total.toFixed(2)}</td>
+                  <td>Real</td>
+                  <td>Editar/Excluir</td>
+                </tr>);
+            })}
+          </tbody>
         </table>
       </div>
     );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Wallet.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.shape).isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
